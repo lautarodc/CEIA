6601a236254160339a714552b0254ce5037c613f
lautarodc <ldc0295@gmail.com>
2020-07-31 20:34:57 -0300

    Matematica para IA - Filtro de Kalman - MSE Metric

---
+++ Matematica para IA/Simulaciones del Filtro de Kalman/Metric.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+class Metric(object):
+    def __call__(self, target, prediction):
+        return NotImplemented
+
+
+class MSE(Metric):
+    def __call__(self, target, prediction):
+        n = target.size
+        return np.sum((target - prediction) ** 2) / n
